a45c24a706f20c73cd92006a15db7d4da456e2cd
kerbychuable <kerbychua@gmail.com>
2021-12-27 02:05:53 +0800

    Game constants

---
+++ checkers/constants.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+pygameInfo = pygame.display.Info()
+pygame.quit()
+
+# GUI DISPLAY CONSTANTS
+WIDTH = HEIGHT = min(pygameInfo.current_w, pygameInfo.current_h)
+ROWS = COLS = 8
+TILE_SIZE = WIDTH//COLS
+
+# COLORS
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+LIGHTBROWN = (255, 191, 128)
+BROWN = (153, 77, 0)
+BLUE = (0, 0, 255)
