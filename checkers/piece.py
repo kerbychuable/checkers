ba89a5359c89b00dc6ee53af6409fe4d7cde35dd
kerbychuable <kerbychua@gmail.com>
2021-12-27 04:36:20 +0800

    fix indention error

--- checkers/piece.py
+++ checkers/piece.py
@@ -14,24 +14,24 @@ class Piece:
     self.y = 0
     self.set_pos()
 
-    def set_pos(self):
-      self.x = TILE_SIZE * self.col + TILE_SIZE // 2
-      self.y = TILE_SIZE * self.row + TILE_SIZE // 2
+  def set_pos(self):
+    self.x = TILE_SIZE * self.col + TILE_SIZE // 2
+    self.y = TILE_SIZE * self.row + TILE_SIZE // 2
 
-    def set_king(self):
-      self.king = True
-    
-    def draw(self, window):
-      circRadius = TILE_SIZE // 2 - PADDING
-      pygame.draw.circle(window, GREY, (self.x, self.y), circRadius + OUTLINE) # Draw piece outline
-      pygame.draw.circle(window, self.color, (self.x, self.y), circRadius) # Draw piece
-      # if self.king: # King piece
-      #   win.blit()
+  def set_king(self):
+    self.king = True
+  
+  def draw(self, window):
+    circRadius = TILE_SIZE // 2 - self.PADDING
+    pygame.draw.circle(window, GREY, (self.x, self.y), circRadius + self.OUTLINE) # Draw piece outline
+    pygame.draw.circle(window, self.color, (self.x, self.y), circRadius) # Draw piece
+    # if self.king: # King piece
+    #   win.blit()
 
-    def move(self, row, col):
-      self.row = row
-      self.col = col
-      self.set_pos()
-    
-    def __repr__(self):
-      return str(self.color)
+  def move(self, row, col):
+    self.row = row
+    self.col = col
+    self.set_pos()
+  
+  def __repr__(self):
+    return str(self.color)
